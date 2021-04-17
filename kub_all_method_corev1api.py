023235e044cd156098b3f17e02a21438869783f0
Deepak Singh <Deepak.Singh2@in.ey.com>
2021-04-17 15:09:40 +0530

    'print'

--- kub_all_method_corev1api.py
+++ kub_all_method_corev1api.py
@@ -16,13 +16,19 @@ def main():
 def list_of_all_method_from_corev1api():
     # config.load_incluster_config()
     # from kubernetes.client import Configuration
+    '''
+    	Notice that getmembers returns a list of 2-tuples. 
+    	The first item is the name of the member, 
+    	the second item is the value.
+    '''
     import inspect
     from kubernetes import config
     from kubernetes.client.api import core_v1_api
     config.load_kube_config()
     core_v1 = core_v1_api.CoreV1Api()
     list_of_method_corev1api = inspect.getmembers(core_v1, predicate=inspect.ismethod)
-    len(list_of_method_corev1api)
+    print(f'Total count of method is :{len(list_of_method_corev1api)}')
+    print(f'{list_of_method_corev1api}')
 
 
 if __name__ == '__main__':
