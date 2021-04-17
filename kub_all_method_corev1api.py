53a3a4a32fbcde3614903e49a7f3c5a456079434
Deepak Singh <Deepak.Singh2@in.ey.com>
2021-04-17 15:22:59 +0530

    'added'

--- kub_all_method_corev1api.py
+++ kub_all_method_corev1api.py
@@ -21,15 +21,16 @@ def list_of_all_method_from_corev1api():
         The first item is the name of the member, 
         the second item is the value.
     '''
-    # import inspect
+    import inspect
     from kubernetes import config
     from kubernetes.client.api import core_v1_api
     config.load_kube_config()
     core_v1 = core_v1_api.CoreV1Api()
-    # list_of_method_corev1api = inspect.getmembers(core_v1, predicate=inspect.ismethod)
-    list_of_method_corev1api = [func for func in dir(core_v1) if callable(getattr(core_v1, func))]
-    print(f'{list_of_method_corev1api}')
-    print(f'Total count of method is :{len(list_of_method_corev1api)}')
+    list_of_method_corev1api = inspect.getmembers(core_v1, predicate=inspect.ismethod)
+    # list_of_method_corev1api = [func for func in dir(core_v1) if callable(getattr(core_v1, func))]
+    list_of_method_corev1api_final = list(map(lambda b:b[0],list_of_method_corev1api))
+    print(f'{list_of_method_corev1api_final}')
+    print(f'Total count of method is :{len(list_of_method_corev1api_final)}')
 
 
 if __name__ == '__main__':
