faa2fdffd901dcdcace404fcd98ffaf871766b3e
Deepak Singh <Deepak.Singh2@in.ey.com>
2021-04-17 15:27:00 +0530

    'added'

--- kub_all_method_corev1api.py
+++ kub_all_method_corev1api.py
@@ -1,18 +1,4 @@
 
-def main():
-    from kubernetes import client, config
-    # config.load_incluster_config()
-    config.load_kube_config()
-
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
-
-
 def list_of_all_method_from_corev1api():
     # config.load_incluster_config()
     # from kubernetes.client import Configuration
