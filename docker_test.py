a3ee5981bf040966dc2521efcc17ab9069ee26df
inspector007 <ds1990singh@yahoo.com>
2021-09-02 02:04:37 +0000

    added create container

--- docker_test.py
+++ docker_test.py
@@ -4,6 +4,10 @@ client = docker.APIClient(base_url='unix://var/run/docker.sock')
 AUTH_CODE = {'username':'inspector007','password':'Deep@1234'}
 try:
 	client.login(username="inspector007",password="Deep@1234",registry="https://index.docker.io/v1/")
-	client.push(repository='inspector007/hello-world-testing1',tag='0.4',auth_config = AUTH_CODE)
+	import pdb;pdb.set_trace()
+	container = client.create_container('inspector007/hello-world-testing1:0.1', detach=True)
+	response_text = client.commit(container=container,repository="inspector007/hello-world-testing1",tag='0.9')
+	print(response_text)
+	client.push(repository='inspector007/hello-world-testing1',tag='0.9',auth_config = AUTH_CODE)
 except Exception as e:
 	print(f'error occured {e}')
